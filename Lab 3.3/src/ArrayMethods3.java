6e6bf56a9c3c2c0a3aca687e6c067f831ec50ae8
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-02-14 09:20:58 -0500

    2

--- Lab 3.3/src/ArrayMethods3.java
+++ Lab 3.3/src/ArrayMethods3.java
@@ -3,16 +3,26 @@ public class ArrayMethods3 {
 
 	public static String[] mergeSort(String[] list)
 	{
-		s
+		
 	}
 	
 	public static int partition(int[] list1, int front, int back)
 	{
+		front = list1[0];
+		back = list1[list1.length-1];
 		
 	}
 	
 	public static void quickSort(int[] list1, int front, int back)
 	{
-		
+		//Base Case
+		if(back > front)
+		{
+			int pivdex = partition(list1, front, back);
+			//Left Side
+			quickSort(list1, front, pivdex - 1);
+			//Right Side
+			quickSort(list1, pivdex + 1, back);
+		}
 	}
 }
